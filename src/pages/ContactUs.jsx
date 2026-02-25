39e293f764f971866364e2e207e57e89a2110e81
Arpit Agarwal <aarpit688@gmail.com>
2026-02-25 22:17:10 +0530

    Updated Social Media Links

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -628,7 +628,7 @@ const ContactUsSection = () => {
             <div className="mt-12 pt-8 border-t border-white/5">
               <h4 className="text-white font-medium mb-5">Follow Us</h4>
               <div className="flex flex-wrap gap-3">
-                <a href="https://www.facebook.com/profile.php?id=100086626928653" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-[#1877F2] hover:text-white transition-all">
+                <a href="https://www.facebook.com/aynzenix" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-[#1877F2] hover:text-white transition-all">
                   <Facebook className="w-5 h-5" />
                 </a>
                 <a href="https://www.linkedin.com/in/aynzenix-private-limited-5ba173390" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-[#0A66C2] hover:text-white transition-all">
@@ -637,12 +637,12 @@ const ContactUsSection = () => {
                 <a href="https://www.instagram.com/aynzenix/" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-gradient-to-br hover:from-pink-600 hover:to-purple-600 hover:text-white transition-all">
                   <Instagram className="w-5 h-5" />
                 </a>
-                <a href="https://x.com/aynzenixIndia" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-black hover:text-white transition-all">
+                {/* <a href="https://x.com/aynzenixIndia" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-black hover:text-white transition-all">
                   <FaXTwitter className="w-5 h-5" />
                 </a>
                 <a href="https://www.youtube.com/@aynzenix-pvt-ltd" target="_blank" rel="noreferrer" className="w-11 h-11 flex items-center justify-center rounded-xl bg-white/5 border border-white/5 text-slate-400 hover:bg-[#FF0000] hover:text-white transition-all">
                   <Youtube className="w-5 h-5" />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
